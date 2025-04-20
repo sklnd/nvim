e669db0b2b4508e38e8ef2a7db60312ad9c237db
Chris Skalenda <chris@skalenda.org>
2025-04-20 13:21:03 -0600

    Remove relative number

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,7 +14,6 @@ local opt = vim.o
 opt.path = vim.o.path .. '**'
 
 opt.number = true
-opt.relativenumber = true
 opt.cursorline = true
 opt.lazyredraw = true
 opt.showmatch = true -- Highlight matching parentheses, etc
