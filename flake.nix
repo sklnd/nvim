ecefac819e7eab6b3dedd3c5b45873e20195b7ac
Chris Skalenda <chris@skalenda.org>
2025-04-27 20:23:55 -0600

    Fix basedpyright configuration

--- flake.nix
+++ flake.nix
@@ -55,6 +55,8 @@
           # allow quick iteration of lua configs
           ln -Tfns $PWD/nvim ~/.config/nvim-dev
         '';
+        LOCALE_ARCHIVE = if pkgs.stdenv.isLinux then "${pkgs.glibcLocales}/lib/locale/locale-archive" else "";
+        LC_ALL = if pkgs.stdenv.isLinux then "en_US.utf8" else "en_US.UTF-8" ;
       };
     in {
       packages = rec {
