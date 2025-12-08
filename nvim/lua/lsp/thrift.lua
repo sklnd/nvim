130371ae63847f40c03cd58f953e4f061af894e5
Chris Skalenda <chris@skalenda.org>
2025-12-07 19:59:35 -0700

    Fix thrift.lua negation

--- nvim/lua/lsp/thrift.lua
+++ nvim/lua/lsp/thrift.lua
@@ -5,12 +5,12 @@ local capabilities = require('user.lsp').make_client_capabilities()
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'thrift',
   callback = function()
-    local root_dir = vim.fs.root(0, { '.git' })
-
     -- determine if the rapacity binary is installed
-    if not vim.fn.executable('rapacity') == 1 then
+    if vim.fn.executable('rapacity') ~= 1 then
       return
     end
+
+    local root_dir = vim.fs.root(0, { '.git' })
 
     vim.lsp.start {
       root_dir = root_dir,
