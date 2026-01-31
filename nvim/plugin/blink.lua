d03f5445efd5601cc357b18992c9297e2f24c8db
Chris Skalenda <chris@skalenda.org>
2026-01-30 20:14:38 -0700

    Configure providers in blink.cmp

--- nvim/plugin/blink.lua
+++ nvim/plugin/blink.lua
@@ -5,6 +5,27 @@ require('blink.cmp').setup {
       show_with_menu = false,
     },
   },
+  sources = {
+    default = { 'lsp', 'path', 'snippets', 'buffer' },
+    providers = {
+      snippets = {
+        min_keyword_length = 2,
+        score_offset = 4,
+      },
+      lsp = {
+        min_keyword_length = 3,
+        score_offset = 3,
+      },
+      path = {
+        min_keyword_length = 3,
+        score_offset = 2,
+      },
+      buffer = {
+        min_keyword_length = 5,
+        score_offset = 1,
+      },
+    },
+  },
   documentation = {
     auto_show = true,
     auto_show_delay = 200,
