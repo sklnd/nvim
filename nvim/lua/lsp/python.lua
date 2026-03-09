f2cef35bb43558edd92c156a2d73c67c478ae69a
Chris Skalenda <chris@skalenda.org>
2026-03-08 18:01:47 -0600

    Try using ty as a lsp

--- nvim/lua/lsp/python.lua
+++ nvim/lua/lsp/python.lua
@@ -5,36 +5,46 @@ vim.api.nvim_create_autocmd('FileType', {
   callback = function()
     local root_dir = vim.fs.root(0, { 'pyproject.toml', 'setup.py', '.git', 'requirements.txt' })
 
-    -- basedpyright
-    vim.lsp.start {
-      name = 'basedpyright',
-      cmd = { 'basedpyright-langserver', '--stdio' },
-      root_dir = root_dir,
-      capabilities = capabilities,
-      settings = {
-        basedpyright = {
-          disableOrganizeImports = true,
-          analysis = {
-            diagnosticMode = 'openFilesOnly',
-            inlayHints = {
-              genericTypes = true,
-            },
-            diagnosticSeverityOverrides = {
-              reportAny = 'hint',
-              reportExplicitAny = 'hint',
-              reportImplicitStringConcatenation = 'hint',
-              reportPrivateImportUsage = 'hint',
-              reportPrivateUsage = 'hint',
-              reportUnannotatedClassAttribute = 'none',
-              reportUnknownArgumentType = 'none',
-              reportUnknownMemberType = 'none',
-              reportUnknownVariableType = 'none',
-              reportUnusedCallResult = 'none',
+    if vim.fn.executable('ty') == 1 then
+      -- ty
+      vim.lsp.start {
+        name = 'ty',
+        cmd = { 'ty', 'server' },
+        root_dir = root_dir,
+        capabilities = capabilities,
+      }
+    else
+      -- basedpyright
+      vim.lsp.start {
+        name = 'basedpyright',
+        cmd = { 'basedpyright-langserver', '--stdio' },
+        root_dir = root_dir,
+        capabilities = capabilities,
+        settings = {
+          basedpyright = {
+            disableOrganizeImports = true,
+            analysis = {
+              diagnosticMode = 'openFilesOnly',
+              inlayHints = {
+                genericTypes = true,
+              },
+              diagnosticSeverityOverrides = {
+                reportAny = 'hint',
+                reportExplicitAny = 'hint',
+                reportImplicitStringConcatenation = 'hint',
+                reportPrivateImportUsage = 'hint',
+                reportPrivateUsage = 'hint',
+                reportUnannotatedClassAttribute = 'none',
+                reportUnknownArgumentType = 'none',
+                reportUnknownMemberType = 'none',
+                reportUnknownVariableType = 'none',
+                reportUnusedCallResult = 'none',
+              },
             },
           },
         },
-      },
-    }
+      }
+    end
 
     -- ruff
     vim.lsp.start {
