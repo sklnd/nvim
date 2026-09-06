40cb8b56bf84cb8838544e5074f138193e044951
Chris Skalenda <chris.skalenda@joinhonor.com>
2026-09-06 09:34:47 -0600

    limit lightbulb action kinds
    Some LSPs provide codeactions on every line, like ruff formatting the
    file or organizing imports.

--- nvim/plugin/nvim-lightbulb.lua
+++ nvim/plugin/nvim-lightbulb.lua
@@ -1,5 +1,8 @@
+-- Shows a lightbulb icon when a line has a lsp codeaction
+
 require('nvim-lightbulb').setup {
   autocmd = { enabled = true },
+  action_kinds = { 'quickfix', 'refactor' },
   sign = {
     enabled = true,
   },
